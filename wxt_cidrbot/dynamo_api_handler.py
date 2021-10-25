1fa42b2f09e012df9d1d499c1abb2e7897df35e1
ppajersk <ppajersk@cisco.com>
2021-10-25 19:39:16 -0400

    Added multiroom functionality Updated interactions to dynamodb to interact with only 1 Table Added issue info command Restricted bot commands to rooms only

--- wxt_cidrbot/dynamo_api_handler.py
+++ wxt_cidrbot/dynamo_api_handler.py
@@ -1,7 +1,7 @@
 import logging
 import os
 import boto3
-from boto3.dynamodb.conditions import Key, Attr
+from boto3.dynamodb.conditions import Key
 
 
 class dynamoapi:
@@ -10,159 +10,253 @@ class dynamoapi:
         logging.basicConfig(level=os.environ.get("LOGLEVEL", "DEBUG"))
         self.logging = logging.getLogger()
 
-        # Init global vars, which immediately get converted to objects in exactly 4 lines of code. This is pylinting's fault! Not my choice :((
         self.dynamodb = ""
         self.table = ""
 
     def get_dynamo(self):
         self.dynamodb = boto3.resource('dynamodb')
+        self.table = self.dynamodb.Table('cidrbot-users-repos')
+
+    def get_dynamo_legacy_table(self):
+        self.dynamodb = boto3.resource('dynamodb')
         self.table = self.dynamodb.Table('cidrbot_user_message_preferences')
 
-    # Start connection to dynamo. This is done as a function and not in the init because dynamo isn't necessarily called each time the lambda function is run
-    def dynamo_db(self, request, name, status, person_id, full_name):
+    def create_room(self, room_id, members, id_list):
+        self.get_dynamo()
+        self.table.put_item(Item={'room_id': room_id, 'users': {}, 'repos': {}, 'webhook_ids': id_list})
+
+        for member in members:
+            email = member['user_email']
+            first_name = member['first_name']
+            dup_status = member['duplicate']
+            person_id = member['person_id']
+
+            self.table.update_item(
+                Key={'room_id': room_id},
+                UpdateExpression="set #user.#username= :name",
+                ExpressionAttributeNames={
+                    '#user': 'users',
+                    '#username': email
+                },
+                ExpressionAttributeValues={
+                    ':name': {
+                        'reminders_enabled': 'off',
+                        'dup_status': dup_status,
+                        'first_name': first_name,
+                        'person_id': person_id
+                    }
+                }
+            )
+
+            self.logging.debug("added user")
+            self.logging.debug(email)
+
+    def delete_room(self, room_id):
+        self.get_dynamo()
+        self.table.delete_item(Key={'room_id': room_id})
+
+        try:
+            response = self.table.query(KeyConditionExpression=Key('room_id').eq(room_id))
+            self.logging.debug("Room could not be deleted")
+            self.logging.debug(response)
+        except Exception:
+            self.logging.debug("Room deleted")
+
+    def clean_username(self, name):
+        name = str(name)
+        if "@" in name:
+            name = name.split("@", 1)[0]
+
+        return name
+
+    def get_all_ids(self):
+        self.get_dynamo()
+        all_room_ids = self.table.scan()
+
+        ids = []
+        for i in all_room_ids['Items']:
+            ids.append(i['room_id'])
+
+        self.logging.debug(ids)
+        return ids
+
+    def user_dict(self, room_id):
+        self.get_dynamo()
+        response = self.table.query(KeyConditionExpression=Key('room_id').eq(room_id))
+
+        return response['Items'][0]['users']
+
+    def get_webhooks(self, room_id):
+        self.get_dynamo()
+
+        response = self.table.query(KeyConditionExpression=Key('room_id').eq(room_id))
+
+        return response['Items'][0]['webhook_ids']
+
+    def get_repositories(self, room_id):
+        self.get_dynamo()
+        response = self.table.query(KeyConditionExpression=Key('room_id').eq(room_id))
+
+        repo_dict = response['Items'][0]['repos']
+        repo_list = []
+
+        for i in repo_dict:
+            repo_list.append(i)
+
+        return repo_list
+
+    def update_repo_list(self, name, request, room_id):
+        self.get_dynamo()
+
+        response = self.table.query(KeyConditionExpression=Key('room_id').eq(room_id))
+
+        db_repo_name = None
+        if name in response['Items'][0]['repos']:
+            db_repo_name = name
+
+        if request == "add repo":
+            if db_repo_name is None:
+                self.table.update_item(
+                    Key={'room_id': room_id},
+                    UpdateExpression="set #repo.#reponame= :name",
+                    ExpressionAttributeNames={
+                        '#repo': 'repos',
+                        '#reponame': name
+                    },
+                    ExpressionAttributeValues={':name': ''}
+                )
+
+                return f"Successfuly added repo: {name}"
+
+            return f"Repo: {name} already exists"
+
+        if db_repo_name is not None:
+            self.table.update_item(
+                Key={'room_id': room_id},
+                UpdateExpression="REMOVE #repo.#reponame",
+                ExpressionAttributeNames={
+                    '#repo': 'repos',
+                    '#reponame': name
+                }
+            )
+
+            return f"Successfuly removed repo: {name}"
+        return f"Cannot find repo: {name}"
+
+    def get_notif_users(self):
+        self.get_dynamo()
+        all_rooms = self.table.scan()
+
+        return all_rooms
+
+    def get_user_info(self, name, room_id):
         self.get_dynamo()
         name = self.clean_username(name)
 
-        if request == "repos":
-            return self.get_repositories()
-        if request == "all_users":
-            return self.user_dict()
-        if request == "notif_users":
-            return self.get_notif_users()
-        if request == "user_info":
-            return self.get_user_info(name)
-        if request == "create_user":
-            return self.create_user(name, status, person_id, full_name)
-        if request == "update_user":
-            return self.update_user(name, status, person_id, full_name)
-        if request == "delete_user":
-            return self.delete_user(name)
-        if request == "add repo":
-            return self.update_repo_list(name, request)
-        if request == "remove repo":
-            return self.update_repo_list(name, request)
-        return "No request type found"
+        response = self.table.query(KeyConditionExpression=Key('room_id').eq(room_id))
 
-    # Remove the @ cisco tag from the username. If users join a cidrbot room who don't belong to cisco/don't have the @ tag, this can be changed to clean that
-    def clean_username(self, name):
-        name = str(name)
-        if "@cisco.com" in name:
-            name = name.split("@cisco.com")[0]
-        return name
+        return response['Items'][0]['users'][name]
 
-    def user_dict(self):
-        all_users = self.table.scan()
+    def create_user(self, name, person_id, full_name, room_id):
+        self.get_dynamo()
+        name = self.clean_username(name)
 
-        return all_users
-
-    def get_repositories(self):
-        self.table = self.dynamodb.Table('cidrbot_repos')
-        response = self.table.scan()
-        repo_list = ""
-        for repo in response['Items']:
-            repo_list += repo['Repositories'] + ","
-        repo_list = repo_list[:-1]
-        return repo_list.split(',')
-
-    def update_repo_list(self, name, request):
-        self.table = self.dynamodb.Table('cidrbot_repos')
-        db_repo_name = None
-        response = self.table.query(KeyConditionExpression=Key('Repositories').eq(name))
-
-        if len(response['Items']) > 0:
-            db_repo_name = response['Items'][0]['Repositories']
-
-        if request == "remove repo":
-            if db_repo_name is not None and db_repo_name == name:
-                self.table.delete_item(Key={'Repositories': name})
-                return f"Successfuly removed repo: {name}"
-            return f"Cannot find repo: {name}"
-
-        if db_repo_name == name:
-            return f"Repo: {name} already exists"
-
-        self.table.put_item(Item={'Repositories': name})
-        return f"Successfuly added repo: {name}"
-
-    def get_notif_users(self):
-        response = self.table.scan(FilterExpression=Attr('reminders_enabled').eq('on'))
-
-        return response
-
-    def get_user_info(self, name):
-        response = self.table.query(KeyConditionExpression=Key('User').eq(name))
-
-        return response
-
-    def create_user(self, name, status, person_id, full_name):
         first_name = full_name.split(" ")[0].lower()
 
-        response = self.table.scan()
+        response = self.table.query(KeyConditionExpression=Key('room_id').eq(room_id))
 
         dup_status = False
-        for user in response['Items']:
-            if first_name == user['first_name']:
+        for user in response['Items'][0]['users']:
+            if response['Items'][0]['users'][user]['first_name'] == first_name:
                 dup_status = True
                 self.table.update_item(
-                    Key={'User': user['User']},
-                    UpdateExpression='SET dup_status = :dup',
-                    ExpressionAttributeValues={':dup': True}
+                    Key={'room_id': room_id},
+                    UpdateExpression="set #user.#username.#dup= :name",
+                    ExpressionAttributeNames={
+                        '#user': 'users',
+                        '#username': name,
+                        '#dup': 'dup_status'
+                    },
+                    ExpressionAttributeValues={':name': dup_status}
                 )
 
-        self.table.put_item(
-            Item={
-                'User': name,
-                'person_id': person_id,
-                'reminders_enabled': status,
-                'first_name': first_name,
-                'dup_status': dup_status
+        self.table.update_item(
+            Key={'room_id': room_id},
+            UpdateExpression="set #user.#username= :name",
+            ExpressionAttributeNames={
+                '#user': 'users',
+                '#username': name
+            },
+            ExpressionAttributeValues={
+                ':name': {
+                    'reminders_enabled': 'off',
+                    'dup_status': dup_status,
+                    'first_name': first_name,
+                    'person_id': person_id
+                }
             }
         )
 
-    def update_user(self, name, status, person_id, full_name):
-        response = self.get_user_info(name)
-        self.logging.debug(status)
-        if len(response['Items']) == 0:
-            self.create_user(name, status, person_id, full_name)
-        else:
+    def update_user(self, name, status, person_id, room_id):
+        self.get_dynamo()
+        name = self.clean_username(name)
+
+        for room in room_id:
+            response = self.table.query(KeyConditionExpression=Key('room_id').eq(room))
+
+            if name in response['Items'][0]['users']:
+                if person_id in response['Items'][0]['users'][name]['person_id']:
+                    self.table.update_item(
+                        Key={'room_id': room},
+                        UpdateExpression="set #user.#username.#reminders = :name",
+                        ExpressionAttributeNames={
+                            '#user': 'users',
+                            '#username': name,
+                            '#reminders': 'reminders_enabled'
+                        },
+                        ExpressionAttributeValues={':name': status}
+                    )
+                    self.logging.debug("updated")
+
+        return f"Successfully turned {status} reminders for {name}"
+
+    def delete_user(self, name, room_id):
+        self.get_dynamo()
+        name = self.clean_username(name)
+
+        response = self.table.query(KeyConditionExpression=Key('room_id').eq(room_id))
+
+        if len(response['Items'][0]['users'][name]['first_name']) > 0:
+
             self.table.update_item(
-                Key={'User': name},
-                UpdateExpression='SET reminders_enabled = :status',
-                ExpressionAttributeValues={':status': status}
+                Key={'room_id': room_id},
+                UpdateExpression="REMOVE #user.#username",
+                ExpressionAttributeNames={
+                    '#user': 'users',
+                    '#username': name
+                }
             )
 
-            if response['Items'][0]['person_id'] is not person_id:
-                self.table.update_item(
-                    Key={'User': name},
-                    UpdateExpression='SET person_id = :id',
-                    ExpressionAttributeValues={':id': person_id}
-                )
+            response_check = self.table.query(KeyConditionExpression=Key('room_id').eq(room_id))
 
-        if status == "on":
-            return f"Reminders enabled for user: {name}"
-        return f"Reminders disabled for user: {name}"
+            dup_counter = 0
 
-    def delete_user(self, name):
-        # Remove dup status if only 1 person remains with it
-        response = self.table.query(KeyConditionExpression=Key('User').eq(name))
+            user = ''
+            for user in response_check['Items'][0]['users']:
+                if response['Items'][0]['users'][name]['first_name'] == response_check['Items'][0]['users'][user][
+                    'first_name']:
+                    if name != user:
+                        dup_counter += 1
 
-        if len(response['Items']) > 0:
-            self.table.delete_item(Key={'User': name})
-
-        all_users = self.table.scan()
-        dup_counter = 0
-
-        user = ''
-        for user in all_users['Items']:
-            if response['Items'][0]['first_name'] == user['first_name']:
-                if response['Items'][0]['User'] != user['User']:
-                    dup_counter += 1
-
-        # Change dup status to False if 1 user remains
-        if len(user) > 0:
-            if 0 < dup_counter < 2:
-                self.table.update_item(
-                    Key={'User': user['User']},
-                    UpdateExpression='SET dup_status = :dup',
-                    ExpressionAttributeValues={':dup': False}
-                )
+            if len(user) > 0:
+                if 0 < dup_counter < 2:
+                    self.table.update_item(
+                        Key={'room_id': room_id},
+                        UpdateExpression="set #user.#username.#dup= :name",
+                        ExpressionAttributeNames={
+                            '#user': 'users',
+                            '#username': user,
+                            '#dup': 'dup_status'
+                        },
+                        ExpressionAttributeValues={':name': False}
+                    )
