124621223b572c755e6bf8ac18fca36fc7aefb44
ppajersk <ppajersk@cisco.com>
2021-11-05 21:19:53 -0400

    Installation removed webhook implemented. When an app is un-installed, it is removed from dynamodb as well as any associated repos for a specific room

--- wxt_cidrbot/git_webhook_handler.py
+++ wxt_cidrbot/git_webhook_handler.py
@@ -30,6 +30,7 @@ class gitwebhook:
         self.Api = WebexTeamsAPI()
         self.dynamodb = ""
         self.table = ''
+        self.room_id = ''
 
     def webhook_request(self, event):
         json_string = json.loads((event["body"]))
@@ -76,6 +77,15 @@ class gitwebhook:
 
                 self.Api.messages.create(toPersonId=person_id, markdown=message)
 
+        elif event_action == 'deleted':
+            removed_repos = self.delete_installation(installation_id, github_login_id)
+            message_uninstall = "A Cidr installation was just removed from this room. \n The following repos are no longer avaliable \n"
+
+            if len(removed_repos) > 0:
+                for repo in removed_repos:
+                    message_uninstall += f" - " + repo + "\n"
+
+                self.Api.messages.create(self.room_id, markdown=message_uninstall)    
 
     def edit_repo(self, room_id, repo, token, request):
 
@@ -106,6 +116,35 @@ class gitwebhook:
                         '#repo': 'repos',
                         '#reponame' : repo
                     })
+
+    def delete_installation(self, installation_id, github_login_id):
+        event_info = self.check_installation(installation_id, github_login_id)
+
+        self.room_id = event_info[0]['room_id']
+        token = event_info[0]['access_token']
+
+        self.table.delete_item(Key={'installation_id': str(installation_id)})
+        self.table = self.dynamodb.Table('cidrbot-users-repos')
+
+        response = self.table.query(
+            KeyConditionExpression=Key('room_id').eq(self.room_id))
+
+        removed_repo_list = []
+
+        for repo in response['Items'][0]['repos']:
+            self.logging.debug("checking repo")
+            if response['Items'][0]['repos'][repo] == token:
+                removed_repo_list.append(repo)
+
+                self.table.update_item(
+                    Key={'room_id': self.room_id},
+                    UpdateExpression="REMOVE #repo.#reponame",
+                    ExpressionAttributeNames={
+                        '#repo': 'repos',
+                        '#reponame' : repo
+                    })
+
+        return removed_repo_list
 
 
     def check_installation(self, installation_id, github_id):
